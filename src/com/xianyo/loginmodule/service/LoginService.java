eca3050e1b6552864bcc43c73486b161425468d5
obsession707 <394099679@qq.com>
2017-12-11 22:33:09 +0800

    加入我自己之前写的管理员
    至于这个xianyo有没有用我也不知道...

--- src/com/xianyo/loginmodule/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.xianyo.loginmodule.service;
-
-import com.xianyo.loginmodule.dao.pojo.LoginData;
-import com.xianyo.loginmodule.dao.pojo.UserData;
-
-public interface LoginService {
-    UserData Login(LoginData userdata);
-}
--- src/com/xianyo/loginmodule/service/RegisterService.java
+++
@@ -1,7 +0,0 @@
-package com.xianyo.loginmodule.service;
-
-import com.xianyo.loginmodule.dao.pojo.UserData;
-
-public interface RegisterService {
-    boolean Register(UserData user);
-}
